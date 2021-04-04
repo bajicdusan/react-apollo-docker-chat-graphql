3a29807453f572c22bb1a64e6420dfda50b894bf
bajicdusan <63374780+bajicdusan@users.noreply.github.com>
2021-04-05 00:49:40 +0200

    registration-logic-and-validation
    Registration logic and validation

--- chat-graphql/graphql/resolvers.js
+++ chat-graphql/graphql/resolvers.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+const { UserInputError } = require('apollo-server');
 const { User } = require('../models');
 
 module.exports = {
@@ -11,4 +13,56 @@ module.exports = {
             }
         },
     },
+    Mutation: {
+        register: async (_, args) => {
+            let { username, email, password, confirmPassword } = args
+
+            let errors = {};
+
+            try {
+                // validate input data
+                if (username.trim() === '') errors.username = 'Username must not be empty';
+                if (email.trim() === '') errors.email = 'Email must not be empty';
+                if (password.trim() === '') errors.password = 'Password must not be empty';
+                if (confirmPassword.trim() === '') errors.confirmPassword = 'Repeat password must not be empty';
+
+                if (password !== confirmPassword) errors.confirmPassword = 'Passwords must match';
+
+                // check if username/email exists
+                // const userByUsername = await User.findOne({ where: { username: username } });
+                // const userByEmail = await User.findOne({ where: { email: email } });
+
+                // if (userByUsername) errors.username = 'Username is taken';
+                // if (userByEmail) errors.Email = 'Email is taken';
+
+                if (Object.keys(errors).length > 0) {
+                    throw errors
+                }
+
+                // hash password
+                password = await bcrypt.hash(password, 6);
+                
+                // create user
+                const user = await User.create({
+                    username,
+                    email,
+                    password
+                });
+
+                return user.toJSON();
+                // could also do like
+                // return user;
+            } catch (err) {
+                console.log(err);
+
+                if (err.name === 'SequelizeUniqueConstraintError') {
+                    err.errors.forEach((e) => (errors[e.path.split('.')[1]] = `${e.path.split('.')[1]} is already taken`));
+                } else if(err.name === 'SequelizeValidationError') {
+                    err.errors.forEach(e => errors[e.path] = e.message);
+                }
+
+                throw new UserInputError('Bad input', { errors });
+            }
+        }
+    }
 };
