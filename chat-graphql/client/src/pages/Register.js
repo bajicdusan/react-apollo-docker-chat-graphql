42c1509d0f7a23f4d0b70c99db7b6f91347d17c3
bajicdusan <63374780+bajicdusan@users.noreply.github.com>
2021-04-25 00:40:06 +0200

    005-register-login-page-logic
    Implemented logic in order to allow users to register/login to the site in order to show to the home page.

--- chat-graphql/client/src/pages/Register.js
+++ chat-graphql/client/src/pages/Register.js
@@ -1,18 +1,46 @@
+import { gql, useMutation } from '@apollo/client';
 import React, { useState } from 'react';
 import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-export default function Register() {
+const REGISTER_USER = gql`
+	mutation register(
+		$username: String!
+		$email: String!
+		$password: String!
+		$confirmPassword: String!
+	) {
+		register(
+			username: $username
+			email: $email
+			password: $password
+			confirmPassword: $confirmPassword
+		) {
+			username
+			email
+			createdAt
+		}
+	}
+`;
+
+export default function Register(props) {
 	const [variables, setVariables] = useState({
 		email: '',
 		username: '',
 		password: '',
 		confirmPassword: '',
 	});
+	const [errors, setErrors] = useState({});
+
+	const [registerUser, { loading }] = useMutation(REGISTER_USER, {
+		update: (_, __) => props.history.push('/login'),
+		onError: (err) => setErrors(err.graphQLErrors[0].extensions.errors),
+	});
 
 	const submitRegisterForm = (e) => {
 		e.preventDefault();
 
-		console.log(variables);
+		registerUser({ variables });
 	};
 
 	return (
@@ -21,43 +49,59 @@ export default function Register() {
 				<h1 className="text-center mb-3">Register</h1>
 				<Form onSubmit={submitRegisterForm}>
 					<Form.Group>
-						<Form.Label>Email address</Form.Label>
+						<Form.Label className={errors.email && 'text-danger'}>
+							{errors.email ?? 'Email address'}
+						</Form.Label>
 						<Form.Control
 							type="email"
 							value={variables.email}
+							className={errors.email && 'is-invalid'}
 							onChange={(e) => setVariables({ ...variables, email: e.target.value })}
 						/>
 					</Form.Group>
 					<Form.Group>
-						<Form.Label>Username</Form.Label>
+						<Form.Label className={errors.username && 'text-danger'}>
+							{errors.username ?? 'Username'}
+						</Form.Label>
 						<Form.Control
 							type="text"
 							value={variables.username}
+							className={errors.username && 'is-invalid'}
 							onChange={(e) => setVariables({ ...variables, username: e.target.value })}
 						/>
 					</Form.Group>
 					<Form.Group>
-						<Form.Label>Password</Form.Label>
+						<Form.Label className={errors.password && 'text-danger'}>
+							{errors.password ?? 'Password'}
+						</Form.Label>
 						<Form.Control
 							type="password"
 							value={variables.password}
+							className={errors.password && 'is-invalid'}
 							onChange={(e) => setVariables({ ...variables, password: e.target.value })}
 						/>
 					</Form.Group>
 					<Form.Group>
-						<Form.Label>Confirm Password</Form.Label>
+						<Form.Label className={errors.confirmPassword && 'text-danger'}>
+							{errors.confirmPassword ?? 'Confirm Password'}
+						</Form.Label>
 						<Form.Control
 							type="password"
 							value={variables.confirmPassword}
+							className={errors.confirmPassword && 'is-invalid'}
 							onChange={(e) =>
 								setVariables({ ...variables, confirmPassword: e.target.value })
 							}
 						/>
 					</Form.Group>
 					<div className="text-center">
-						<Button variant="primary" type="submit">
-							Register
+						<Button variant="primary" type="submit" disabled={loading}>
+							{loading ? 'loading...' : 'Register'}
 						</Button>
+						<br />
+						<small>
+							Already have an account? <Link to="/login">Login</Link>
+						</small>
 					</div>
 				</Form>
 			</Col>
