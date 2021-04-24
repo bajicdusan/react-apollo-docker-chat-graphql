548242dcf2a160dc191935977d562c74aa584c9a
bajicdusan <63374780+bajicdusan@users.noreply.github.com>
2021-04-24 18:55:19 +0200

    003-login-and-authentication-logic
    Implemented login and jsonwebtoken authentication logic.

--- chat-graphql/graphql/resolvers.js
+++ chat-graphql/graphql/resolvers.js
@@ -1,68 +1,136 @@
 const bcrypt = require('bcryptjs');
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
+const jwt = require('jsonwebtoken');
+const { Op } = require('sequelize');
+
 const { User } = require('../models');
+const { JWT_SECRET } = require('../config/env.json');
 
 module.exports = {
-    Query: {
-        getUsers: async () => {
-            try {
-                const users = await User.findAll();
-                return users;
-            } catch (error) {
-                console.log(error);
-            }
-        },
-    },
-    Mutation: {
-        register: async (_, args) => {
-            let { username, email, password, confirmPassword } = args
+	Query: {
+		getUsers: async (_, __, context) => {
+			try {
+				let user;
 
-            let errors = {};
+				if (context.req && context.req.headers.authorization) {
+					const token = context.req.headers.authorization.split('Bearer ')[1];
+					jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+						if (err) {
+							throw new AuthenticationError('Unauthenticated');
+						}
+						user = decodedToken;
+					});
+				}
 
-            try {
-                // validate input data
-                if (username.trim() === '') errors.username = 'Username must not be empty';
-                if (email.trim() === '') errors.email = 'Email must not be empty';
-                if (password.trim() === '') errors.password = 'Password must not be empty';
-                if (confirmPassword.trim() === '') errors.confirmPassword = 'Repeat password must not be empty';
+				const users = await User.findAll({
+					where: { username: { [Op.ne]: user.username } },
+				});
+				return users;
+			} catch (err) {
+				console.log(err);
+				throw err;
+			}
+		},
+		login: async (_, args) => {
+			const { username, password } = args;
+			let errors = {};
 
-                if (password !== confirmPassword) errors.confirmPassword = 'Passwords must match';
+			try {
+				if (username.trim() === '') {
+					errors.username = 'username must not be empty';
+				}
 
-                // check if username/email exists
-                // const userByUsername = await User.findOne({ where: { username: username } });
-                // const userByEmail = await User.findOne({ where: { email: email } });
+				if (password.trim() === '') {
+					errors.password = 'password must not be empty';
+				}
 
-                // if (userByUsername) errors.username = 'Username is taken';
-                // if (userByEmail) errors.Email = 'Email is taken';
+				if (Object.keys(errors).length > 0) {
+					throw new UserInputError('bad input', { errors });
+				}
 
-                if (Object.keys(errors).length > 0) {
-                    throw errors
-                }
+				const user = await User.findOne({
+					where: { username },
+				});
 
-                // hash password
-                password = await bcrypt.hash(password, 6);
-                
-                // create user
-                const user = await User.create({
-                    username,
-                    email,
-                    password
-                });
+				if (!user) {
+					errors.username = 'user not found';
+					throw new UserInputError('user not found', { errors });
+				}
 
-                return user.toJSON();
-                // could also do like
-                // return user;
-            } catch (err) {
-                console.log(err);
+				const correctPassword = await bcrypt.compare(password, user.password);
 
-                if (err.name === 'SequelizeUniqueConstraintError') {
-                    err.errors.forEach((e) => (errors[e.path.split('.')[1]] = `${e.path.split('.')[1]} is already taken`));
-                } else if(err.name === 'SequelizeValidationError') {
-                    err.errors.forEach(e => errors[e.path] = e.message);
-                }
+				if (!correctPassword) {
+					errors.password = 'password is incorrect';
+					throw new AuthenticationError('password is incorrect', { errors });
+				}
 
-                throw new UserInputError('Bad input', { errors });
-            }
-        }
-    }
+				const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: 60 * 60 });
+
+				return {
+					...user.toJSON(),
+					createdAt: user.createdAt.toISOString(),
+					token,
+				};
+			} catch (err) {
+				console.log(err);
+				throw err;
+			}
+		},
+	},
+	Mutation: {
+		register: async (_, args) => {
+			let { username, email, password, confirmPassword } = args;
+
+			let errors = {};
+
+			try {
+				// validate input data
+				if (username.trim() === '') errors.username = 'Username must not be empty';
+				if (email.trim() === '') errors.email = 'Email must not be empty';
+				if (password.trim() === '') errors.password = 'Password must not be empty';
+				if (confirmPassword.trim() === '')
+					errors.confirmPassword = 'Repeat password must not be empty';
+
+				if (password !== confirmPassword) errors.confirmPassword = 'Passwords must match';
+
+				// check if username/email exists
+				// const userByUsername = await User.findOne({ where: { username: username } });
+				// const userByEmail = await User.findOne({ where: { email: email } });
+
+				// if (userByUsername) errors.username = 'Username is taken';
+				// if (userByEmail) errors.Email = 'Email is taken';
+
+				if (Object.keys(errors).length > 0) {
+					throw errors;
+				}
+
+				// hash password
+				password = await bcrypt.hash(password, 6);
+
+				// create user
+				const user = await User.create({
+					username,
+					email,
+					password,
+				});
+
+				// could also do like
+				// return user;
+				return user.toJSON();
+			} catch (err) {
+				console.log(err);
+
+				if (err.name === 'SequelizeUniqueConstraintError') {
+					err.errors.forEach(
+						(e) =>
+							(errors[e.path.split('.')[1]] = `${e.path.split('.')[1]} is already taken`)
+					);
+				} else if (err.name === 'SequelizeValidationError') {
+					err.errors.forEach((e) => (errors[e.path] = e.message));
+				}
+
+				throw new UserInputError('Bad input', { errors });
+			}
+		},
+	},
 };
--- chat-graphql/server.js
+++ chat-graphql/server.js
@@ -7,14 +7,16 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+	typeDefs,
+	resolvers,
+	context: (ctx) => ctx,
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+	console.log(`🚀 Server ready at ${url}`);
 
-  sequelize.authenticate()
-  .then(() => console.log('Database connected!'))
-  .catch(err => console.log(err))
+	sequelize
+		.authenticate()
+		.then(() => console.log('Database connected!'))
+		.catch((err) => console.log(err));
 });
